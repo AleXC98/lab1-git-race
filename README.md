07b49083326485cd057658854e1f87bf7d5e1955
Alex Costa <costa98alex@gmail.com>
2020-09-18 17:57:06 +0200

    added documentation

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ Some ideas for obtaining a :gift: if you are the first that:
 User name | NIA | Travis-CI|Score
 ----------|-----|----------|-----
 [UNIZAR-30246-WebEngineering](https://github.com/UNIZAR-30246-WebEngineering/lab1-git-race) |30246 | [![Build Status](https://travis-ci.com/UNIZAR-30246-WebEngineering/lab1-git-race.svg)](https://travis-ci.com/UNIZAR-30246-WebEngineering/lab1-git-race)
-your name | your nia | your Travis-ci status
+Alex Costa | 824298 | [![Build Status](https://travis-ci.org/costaalex/lab1-git-race.svg)](https://travis-ci.org/costaalex/lab1-git-race)
--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -7,15 +7,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Date;
 import java.util.Map;
 
+
+
 @Controller
 public class HelloController {
     @Value("${app.message:Hello World}")
     private String message;
+    @Value("${app.messageType:text}")
+    private String messageType;
+
+    /**
+     * This is a controller method that sets the time key to current Date, the message to the hardcoded message
+     *
+     * and the message type
+     *
+     * @param model MVC
+     * @return the message "wellcome"
+     *
+     */
 
     @GetMapping("/")
     public String welcome(Map<String, Object> model) {
         model.put("time", new Date());
         model.put("message", message);
+        model.put("messageType", messageType);
         return "wellcome";
     }
 }
